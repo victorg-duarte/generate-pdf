b81577c390766dec364bed3ea6e61730b8415e72
victorg-duarte <vitugduarte@gmail.com>
2022-03-03 17:38:59 -0300

    correcoes

--- gera-pdf-backend/index.js
+++ gera-pdf-backend/index.js
@@ -19,55 +19,60 @@ app.get('/', (req, res) => {
 
 app.get(`/generate-pdf`, async (req, res) => {
 
-    const url = 'http:localhost:3001/template/layoutPdf.html'
-    const fileName = 'pdfBackend'
+    try {
+        const url = 'http:localhost:3001/template/layoutPdf.html'
+        const fileName = 'pdfBackend'
 
-    //puppperter: manipular paginas (Web crawling & scraping) 
-    // headless : true - abre o navegador escondido(background)
-    
-    const browser = await puppeteer.launch({ headless: true }) // config default
+        //puppperter: manipular paginas (Web crawling & scraping) 
+        // headless : true - abre o navegador escondido(background)
 
-    // config para debian
-    /*const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: '/usr/bin/google-chrome-stable'
-    })*/
+        const browser = await puppeteer.launch({ headless: true }) // config default
 
-    const page = await browser.newPage()
+        // config para debian
+        /*const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],'
+        })*/
 
-    // abre a pagina com endereço selecionado
-    await page.goto(url, {
-        waitUntil: 'networkidle0' // espera até carrega toda pagina
-    })
+        const page = await browser.newPage()
 
-    // O pupperteer possui uma funcao chamada pdf, que gera um pdf da pagina
-    const pdf = await page.pdf({
-        path: `./generated-pdf/${fileName}.pdf`,
-        printBackground: true,
-        format: 'a4',
-        landscape: false,
-        margin: {
-            top: '100px',
-            bottom: '100px',
-            left: '20px',
-            right: '20px'
-        },
-        displayHeaderFooter: true,
-        headerTemplate: `<p style="font-size: 8px; margin: 0 auto;">
+        // abre a pagina com endereço selecionado
+        await page.goto(url, {
+            waitUntil: 'networkidle0' // espera até carrega toda pagina
+        })
+
+        // O pupperteer possui uma funcao chamada pdf, que gera um pdf da pagina
+        const pdf = await page.pdf({
+            path: `./generated-pdf/${fileName}.pdf`,
+            printBackground: true,
+            format: 'a4',
+            landscape: false,
+            margin: {
+                top: '100px',
+                bottom: '100px',
+                left: '20px',
+                right: '20px'
+            },
+            displayHeaderFooter: true,
+            headerTemplate: `<p style="font-size: 8px; margin: 0 auto;">
           <span>${fileName}</span> - <span class="date"></span></p>`,
-        footerTemplate: `<p style="font-size:10px; margin: 0 auto;"><span
+            footerTemplate: `<p style="font-size:10px; margin: 0 auto;"><span
           class="pageNumber"></span> of <span class="totalPages"></span></p>`,
-        width: '100px',
-    })
+            width: '100px',
+        })
 
-    await browser.close()
+        await browser.close()
 
-    res.type('application/pdf')
-    // res.send(pdf)
+        res.type('application/pdf')
+        // res.send(pdf)
 
-    // pdf downloading
-    res.download(`./generated-pdf/${fileName}.pdf`)
+        // pdf downloading
+        res.download(`./generated-pdf/${fileName}.pdf`)
 
+    } catch (error) {
+        console.log(`\nUm erro foi encontrado: ${error}`);
+        res.status(408).json({ msg: error })
+    }
 })
 
 app.listen(3001, () => console.log('Server is running on port 3001'))
